1ffc4c2e9510a825b1c70867bc7f63b70bf94119
yusuketono <yusuke.tono@di-v.co.jp>
2020-10-16 12:09:30 +0900

    バリデーション_ユーザー管理機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :email, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana]
+    )
+  end
 end
