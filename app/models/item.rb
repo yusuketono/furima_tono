cd2aa87bd25b30cc7000cc3c5ffb98b5966c8be9
yusuketono <yusuke.tono@di-v.co.jp>
2020-10-16 19:03:59 +0900

    ActiveHash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+
+  # active hash関連
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shipping_fee_status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
+
 end
