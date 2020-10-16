426581f39a2063602aeec033426c6de1a25b6497
yusuketono <yusuke.tono@di-v.co.jp>
2020-10-16 18:02:32 +0900

    テスト_ユーザー管理機能

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user do
+    nickname { Faker::Internet.username }
+    password { '1a' + Faker::Internet.password(min_length: 7, max_length: 20) }
+    email { Faker::Internet.email }
+    birth_date { Faker::Date.between_except(from: 20.year.ago, to: 1.year.from_now, excepted: Date.today) }
+    last_name { '田中' }
+    first_name { '太郎' }
+    last_name_kana { 'タナカ' }
+    first_name_kana { 'タロウ' }
+  end
+end
